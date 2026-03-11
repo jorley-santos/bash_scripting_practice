67c34a053081a61f7df3db21e06b6fa9ca67a416
jorley-santos <jorley2552@gmail.com>
2026-03-10 23:15:17 -0300

    Update

--- bash-script-files/mock_scripts/if_statements/if_statement_7.sh
+++ bash-script-files/mock_scripts/if_statements/if_statement_7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -d /home/jubarte/bash-script-study/bash_scripting_practice/bash-script-files/mock_scripts/if_statements ]
+if [ -d /home/if_statements ]
 then
 	echo "The provided folder exists."
 
